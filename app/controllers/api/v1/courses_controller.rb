67af693562a5f774e843a38af01a235dddfd4384
Tim Koar <koartimothy@gmail.com>
2020-04-27 14:29:26 -0400

    finishing auth

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -10,4 +10,14 @@ class Api::V1::CoursesController < ApplicationController
     render json: @course
   end
 
+  def create
+    @course = Course.create(course_params)
+    render json: { course: CourseSerializer.new(@course) }
+  end
+
+  private
+  def course_params
+    params.require(:course).permit(:name, :difficulty, :description, :img_url)
+  end
+
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,26 @@
 class Api::V1::UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
+
+  def profile
+    render json: {user: UserSerializer.new(current_user) },
+    status :accepted
+  end
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
 
   def create
     @user = User.create(user_params)
     if @user.valid?
-      render json: { user: UserSerializer.new(@user) },
+      @token = encode_token(user_id: @user.id)
+      render json: { user: UserSerializer.new(@user), jwt: @token },
       status: :created
     else
       render json: { error: "failed to create user" },
@@ -11,7 +28,7 @@ class Api::V1::UsersController < ApplicationController
   end
 end
 
-  private 
+  private
   def user_params
     params.require(:user).permit(:username, :password, :bio, :img_url)
   end
